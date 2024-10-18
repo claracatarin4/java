619bdda908324ceb2c1b6fddaf5d01a02099409f
Clara Catarina <claracatarina47@gmail.com>
2024-10-18 09:23:06 -0300

    Add files via upload
    teste do uso de while em Java

---
+++ testewhile.java
@@ -0,0 +1,20 @@
+package primeiroprojeto;
+
+public class testewhile {
+
+	public static void main(String[] args) {
+		int a = 1;
+		while( a<= 50) {
+			System.out.println("a = " +a);
+			a++;
+		}
+		
+		int y = 2;
+		do {
+		  System.out.println("y = "+y);
+		  y++;
+		}
+		while ( y<=5);
+	}
+
+}
